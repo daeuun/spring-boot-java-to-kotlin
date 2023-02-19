e3c1063e407b23b8b0a6d28e5acc69c1f8c6e214
daeuun <leeda51925@gmail.com>
2023-02-19 22:56:30 +0900

    refactoring repository by queryDsl

--- src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/loanhistory/UserLoanHistoryRepository.kt
@@ -1,13 +1,5 @@
 package com.group.libraryapp.domain.user.loanhistory
 
-import com.group.libraryapp.dto.book.response.BookStatResponse
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 
-interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long> {
-    fun findByBookNameAndStatus(bookName: String, status: UserLoanStatus): UserLoanHistory?
-
-    fun findAllByStatus(status: UserLoanStatus): List<UserLoanHistory>
-
-    fun countByStatus(status: UserLoanStatus): Long
-}
+interface UserLoanHistoryRepository : JpaRepository<UserLoanHistory, Long>
